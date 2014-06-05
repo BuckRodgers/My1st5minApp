b3d5a165e2cff2b129f579faaa7d41956f07ac6a
Ryan <lee.ryanjohn@gmail.com>
2014-06-05 16:56:10 +0200

    Adding new project to github

---
+++ My1st5minApp/My1st5minApp/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace My1st5minApp.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ My1st5minApp/My1st5minApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="My1st5minApp.WebApiApplication" Language="C#" %>
